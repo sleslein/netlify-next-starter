ae247cc04cf750b16e35e4197811cf311a97c9e2
Shea Leslein <sleslein@freewheel.com>
2020-11-24 16:53:40 -0600

    completed Head Api

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,13 @@
+import Head from 'next/head';
+
 export function Home() {
-  return <div>Welcome, Explorer</div>;
+  return <>
+    <Head>
+      <title>My Next.js Site!</title>
+      <meta name="viewport" content="initial-scale=1.0, width=devicewidth"></meta>
+    </Head> 
+    <div>Welcome, Explorer</div>
+  </>
 }
 
 export default Home;
