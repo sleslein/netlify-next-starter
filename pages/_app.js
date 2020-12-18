96e9f3dffa11dfa1ba1ad990ffef211addd7251e
Shea Leslein <sleslein@freewheel.com>
2020-12-18 13:45:34 -0600

    Completed Mission

---
+++ pages/_app.js
@@ -0,0 +1,22 @@
+import '../styles.css'
+
+export default function Application({Component, pageProps}){
+  return <Component {...pageProps} /> 
+}
+
+//used to meature performance!
+export function reportWebVitals(metric) {
+  switch (metric.name) {
+    case 'FCP': break;
+    case 'LCP': break;
+    case 'CLS': break;
+    case 'FID': break;
+    case 'TTFB': break;
+    case 'Next.js-hydration': break;
+    case 'Next.js-route-change-to-render': break;
+    case 'Next.js-render': 
+      
+      break;
+    default: break;
+  }
+}
---
+++ pages/pokemon/[pokemon].js
@@ -0,0 +1,46 @@
+import Head from 'next/head';
+import styles from './Pokemon.module.css';
+
+function Pokemon({pokemon}) {
+  return (
+  <>
+    <Head>
+      <title>Pokemon: {pokemon?.name}</title>
+      <meta name="viewport" content="initial-scale=1.0, width=devicewidth"></meta>
+    </Head> 
+    <div className={styles.container}>
+      Hello, {pokemon?.name}!
+      <img src={pokemon?.sprites.front_default} />
+    </div>
+  </>
+  ) 
+}
+
+export async function getStaticPaths() {
+  const res = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+  const pokemon = await res.json();
+
+  let paths = pokemon.results.map(p=> {
+    return `/pokemon/${p.name}`;
+  });
+
+  return {
+    paths,
+    fallback: true
+  };
+
+}
+
+// these are pull at build/compile time.... NOT RUN TIME!
+export async function getStaticProps({params}) {
+  const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${params.pokemon}`);
+  const pokemon = await res.json();
+
+  return {
+    props: {
+      pokemon
+    }
+  }
+}
+
+export default Pokemon;
